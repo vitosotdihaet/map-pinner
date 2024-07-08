aa6705bab23979b76d46d7d165799ba8badb98ae
vitosotdihaet <vitostikitos@gmail.com>
2024-07-08 18:35:29 +0300

    Change initial map zoom

--- web/main.js
+++ web/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([55.76, 37.64], 0)
+var map = L.map('map').setView([55.76, 37.64], 5)
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy OpenStreetMap contributors',
