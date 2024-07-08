999015a2785aef505b33ed32d46543020033b734
vitosotdihaet <vitostikitos@gmail.com>
2024-07-08 11:18:03 +0300

    Don't require name, longitude and latitude in point binding

--- package/entities/point.go
+++ package/entities/point.go
@@ -2,7 +2,7 @@ package entities
 
 type Point struct {
 	ID        uint64  `json:"id"`
-	Name      string  `json:"name" binding:"required"`
-	Longitude float64 `json:"longitude" binding:"required"`
-	Latitude  float64 `json:"latitude" binding:"required"`
+	Name      string  `json:"name"`
+	Longitude float64 `json:"longitude"`
+	Latitude  float64 `json:"latitude"`
 }
