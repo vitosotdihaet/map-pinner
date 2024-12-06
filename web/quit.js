f330f6df833b7371e0e8c507a2312f96f0fb18bf
vitosotdihaet <vitostikitos@gmail.com>
2024-12-06 14:16:57 +0300

    Do not allow cursor when a button is deactivated

--- web/quit.js
+++ web/quit.js
@@ -1,4 +1,6 @@
 function quit() {
     localStorage.setItem('jwt', undefined)
+    localStorage.setItem('user', undefined)
+    localStorage.setItem('region', undefined)
     window.location.href = '/static/auth.html'
 }
